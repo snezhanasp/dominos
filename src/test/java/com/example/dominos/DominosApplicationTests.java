dc2ccc6d7ccb37250a37a1723a160b2dd7db6be3
Iva <iva01_rbd@abv.bg>
2022-10-15 16:43:38 +0300

    initial commit

---
+++ src/test/java/com/example/dominos/DominosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dominos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
