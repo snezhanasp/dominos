6ef3d5e7cb38c845231d2925d8c6d4a81bd67afa
snezhana <spasova.sn@gmail.com>
2022-10-23 16:59:43 +0300

    updated pizza specification method

--- src/main/java/com/example/dominos/model/repositories/PizzaSpecificationRepository.java
+++ src/main/java/com/example/dominos/model/repositories/PizzaSpecificationRepository.java
@@ -4,7 +4,10 @@ import com.example.dominos.model.entities.PizzaSpecification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PizzaSpecificationRepository extends JpaRepository<PizzaSpecification, Long> {
-    PizzaSpecification findByDoughTypeAndSize(PizzaSpecification.DoughType doughType, PizzaSpecification.Size size);
+
+    Optional<PizzaSpecification> findBySize_IdAndDoughType_Id(long sizeID, long doughTypeId);
 }
